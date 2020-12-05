8e4edc82a6af906f732b59c9097c1c462d961bd2
Rhisiart <ricardobenfiquista13@gmail.com>
2020-12-05 10:35:26 +0000

    path color in usastatesvg

--- frontend/src/components/Screen/GenderRatio.tsx
+++ frontend/src/components/Screen/GenderRatio.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 import {Slider, Icon} from 'react-native-elements';
-import { GenderRatio, Teste } from '../../api/HttpRequests';
+import { GenderRatio} from '../../api/HttpRequests';
+import UsaSvgComponent from '../Svg/Usa';
 
 interface IGenderRatio{
     'Male': number;
@@ -21,9 +22,11 @@ interface IResponse{
 
 const GenderRatioScreen : React.FC = () => {
     const [isLoading, setLoading] = useState<boolean>(false);
-    const [minYear,setMinYear] = useState<Number>();
-    const [maxYear,setMaxYear] = useState<Number>();
+    const [minYear,setMinYear] = useState<number>();
+    const [yearToStart,setStart] = useState<number>();
+    const [maxYear,setMaxYear] = useState<number>();
     const [stateArray,setStateArray] = useState<IResponse>();
+    const [color, setColor] = useState<string>('#000');
 
     const processData = async () => {
         try{
@@ -33,11 +36,13 @@ const GenderRatioScreen : React.FC = () => {
             {
                 const responseData : IResponse = response.data;
                 if(responseData.maxYear && responseData.minYear && responseData.stateGenderRatio){
-                    console.log(responseData.minYear);
+                    let startValue = responseData.minYear + ((responseData.maxYear - responseData.minYear) / 2);
                     setLoading(true);
                     setStateArray(responseData);
                     setMinYear(responseData.minYear);
                     setMaxYear(responseData.maxYear);
+                    setStart(startValue);
+                    yearPercentages(startValue);
                 }
                 
             }
@@ -49,9 +54,82 @@ const GenderRatioScreen : React.FC = () => {
     useEffect(() => {
         processData()},[]);
 
+    const yearPercentages = (year : number) => {
+        if(stateArray)
+        {
+            for(const state in stateArray['stateGenderRatio'])
+            {
+                //for now
+                if(state === 'WA')
+                {
+                    for(const percentageList of stateArray['stateGenderRatio'][state])
+                    {
+                        if(percentageList.year === year)
+                        {
+                            let colors = percentageList.Male > percentageList.Female ? '#3990e1' : '#FFC0CB';
+                            console.log(colors);
+                            setColor(colors);
+                        } 
+                    }
+                }
+            }
+        }
+        
+    }
+
     return(
         <View>
-            {isLoading ? <View> <Text>{minYear}</Text> <Text>{maxYear}</Text> <Text>{stateArray}</Text> </View>  : <Text>Loading</Text>}
+            {
+                isLoading ? 
+                <View> 
+                    <View>
+                        <UsaSvgComponent color = {color}/>
+                    </View>
+                    <View>
+                        <Text> {minYear} </Text>
+                        <Slider
+                            animateTransitions
+                            animationType="timing"
+                            maximumTrackTintColor="#0073e6"
+                            minimumValue={minYear}
+                            maximumValue={2014} //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                            minimumTrackTintColor="#ccc"
+                            onSlidingComplete={() =>
+                            console.log("onSlidingComplete()")
+                            }
+                            onSlidingStart={() =>
+                            console.log("onSlidingStart()")
+                            }
+                            onValueChange={value =>
+                                yearPercentages(value)
+                            }
+                            orientation="horizontal"
+                            step={1}
+                            style={{ width: "80%", height: 200 }}
+                            thumbStyle={{ height: 20, width: 20 }}
+                            thumbProps={{
+                            children: (
+                                <Icon
+                                name="event"
+                                type="material"
+                                size={20}
+                                reverse
+                                containerStyle={{ bottom: 20, right: 20 }}
+                                color="#0073e6"
+                                />
+                            )
+                            }}
+                            thumbTintColor="#0c0"
+                            thumbTouchSize={{ width: 40, height: 40 }}
+                            trackStyle={{ height: 10, borderRadius: 20 }}
+                            value={yearToStart}
+                        />
+                        <Text>{maxYear}</Text>
+                    </View>
+                </View>  
+                : 
+                <Text>Loading</Text>
+            }
         </View>
     );
 }
